84b4cd3ed9a0ddeafee62ba895268dceb306e1d0
Amna Aftab <amnaaftab94@gmail.com>
2016-03-18 06:38:29 -0400

    Done.

--- javacs-lab02/src/com/flatironschool/javacs/MyArrayList.java
+++ javacs-lab02/src/com/flatironschool/javacs/MyArrayList.java
@@ -62,6 +62,14 @@ public class MyArrayList<E> implements List<E> {
 		if (index < 0 || index > size) {
 			throw new IndexOutOfBoundsException();
 		}
+		if(array[index] != null) {
+			// shift elements forward (assumes array has at least one more spot
+			for(int i = array.length - 1; i > index; i--) {
+				array[i] = array[i-1];
+			}
+		}
+		array[index] = element;
+		size++;
 		// TODO: fill in the rest of this method
 	}
 
@@ -112,7 +120,12 @@ public class MyArrayList<E> implements List<E> {
 	@Override
 	public int indexOf(Object target) {
 		// TODO: fill in this method
-		return 0;
+		for(int i = 0; i < size; i++) {
+			if(equals(target, array[i])) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	/** Checks whether an element of the array is the target.
@@ -182,8 +195,18 @@ public class MyArrayList<E> implements List<E> {
 
 	@Override
 	public E remove(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		E temp = array[index];
+		array[index] = null;
+		for (int i = index; i < size; i++) {
+			array[i] = array[i+1];
+		}
+		array[array.length - 1] = null;
+		size--;
 		// TODO: fill in this method.
-		return null;
+		return temp;
 	}
 
 	@Override
@@ -203,7 +226,12 @@ public class MyArrayList<E> implements List<E> {
 	@Override
 	public E set(int index, E element) {
 		// TODO: fill in this method.
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		E temp = array[index];
+		array[index] = element;
+		return temp;
 	}
 
 	@Override
